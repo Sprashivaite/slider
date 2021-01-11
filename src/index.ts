0af20edec6101abb7c3eccf08b1d93faf16a4f39
Sprashivaite <sanitar.kem@mail.ru>
2021-01-11 22:15:45 +0700

    добавлены новые параметры в config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 let slider = $(".slider").sliderPlugin().data("sliderPlugin");
 
-/*
+
 let vl: HTMLInputElement = document.querySelector(".vl");
 let vl_2: HTMLInputElement = document.querySelector(".vl_2");
-let tooltip: HTMLInputElement = document.querySelector("#tooltip");
 let max: HTMLInputElement = document.querySelector("#max");
 let min: HTMLInputElement = document.querySelector("#min");
 let step: HTMLInputElement = document.querySelector("#step");
+let tooltip: HTMLInputElement = document.querySelector("#tooltip");
+let scale: HTMLInputElement = document.querySelector("#scale");
+
 let orientation = document.querySelector("#orientation");
 let range = document.querySelector("#range");
 
@@ -15,34 +17,34 @@ slider.view.button.div.addEventListener("mousemove", () => {
 });
 
 vl.addEventListener("input", function () {
-  slider.presenter.buttonValue = +vl.value
+  slider.presenter.buttonValue = Number(vl.value)
 });
 if (slider.view.isRangeSlider) {
   slider.view.button_2.div.addEventListener("mousemove", () => {
     vl_2.value = slider.presenter.buttonValue_2 + "";
   });
   vl_2.addEventListener("input", function () {
-    slider.presenter.buttonValue_2 = +vl_2.value
+    slider.presenter.buttonValue_2 = Number(vl_2.value)
   });
 }
 
 max.value = slider.model.max + "";
 max.addEventListener("input", function () {
-  slider.model.max = +max.value;
+  slider.model.max = Number(max.value);
   slider.presenter.updateScaleValues()
   slider.presenter.mouseUp();
   slider.presenter.mouseUp_2();
 });
 min.value = slider.model.min + "";
 min.addEventListener("input", function () {
-  slider.model.min = +min.value;
+  slider.model.min = Number(min.value);
   slider.presenter.updateScaleValues()
   slider.presenter.mouseUp();
   slider.presenter.mouseUp_2();
 });
 step.value = slider.model.step + "";
 step.addEventListener("input", function () {
-  let value: number = Math.abs(+step.value);
+  let value: number = Math.abs(Number(step.value));
   if (value === 0) {
     value = 1;
   }
@@ -51,18 +53,30 @@ step.addEventListener("input", function () {
   slider.presenter.mouseUp_2();
 });
 
-orientation.checked = slider.view.isHorizontal;
-orientation.addEventListener("input", function () {
-  slider.presenter.changeOrientation();
-});
+tooltip.checked = slider.view.isFlag;
 tooltip.addEventListener("input", function () {
-  tooltip.checked ? slider.view.flag.addFlag() : slider.view.flag.removeFlag();
+  tooltip.checked ? slider.view.flag.showFlag() : slider.view.flag.hideFlag();
   if (slider.view.flag_2) {
-    tooltip.checked ? slider.view.flag_2.addFlag() : slider.view.flag_2.removeFlag();
+    tooltip.checked ? slider.view.flag_2.showFlag() : slider.view.flag_2.hideFlag();
   }
 });
-range.checked = slider.view.isRangeSlider;
-range.addEventListener("input", function () {
-  slider.presenter.changeTypeSlider();
+
+scale.checked = slider.view.isScale;
+scale.addEventListener("input", function () {
+  scale.checked ? slider.view.scale.showScale() : slider.view.scale.hideScale();
+ 
 });
-*/
+
+orientation.checked = slider.view.isHorizontal;
+orientation.addEventListener("input", function () {
+ 
+  slider.presenter.changeOrientation();
+  scale.checked = slider.view.isScale;
+  tooltip.checked = slider.view.isFlag; 
+});
+range.checked = slider.view.isRangeSlider;
+range.addEventListener("input", function () { 
+  slider.presenter.changeTypeSlider(); 
+  scale.checked = slider.view.isScale;
+  tooltip.checked = slider.view.isFlag; 
+});
