2d2a6052db1ec45c3c99228ad8b9010a4ea4ff33
Sprashivaite <sanitar.kem@mail.ru>
2021-01-20 23:57:28 +0700

    Создан интерфейс IView

---
+++ src/View/IView.ts
@@ -0,0 +1,18 @@
+interface IView {
+  slider: ViewContainer;
+  button: ViewButton;
+  button_2: ViewButton;
+  isHorizontal: boolean;
+  isRangeSlider: boolean;
+  field: ViewField;
+  flag: ViewFlag;
+  flag_2: ViewFlag;
+  progressBar: ViewProgressBar;
+  scale: ViewScale;
+  handler: ViewHandler;
+  isFlag: boolean;
+  isScale: boolean;
+  scaleQuantity: number;
+  isProgressBar: boolean;
+}
+export default IView 
