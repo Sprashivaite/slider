4116bb8339651ece091534b5a646d68f1bb8e79d
Sprashivaite <sanitar.kem@mail.ru>
2021-01-21 00:55:02 +0700

    изменена директория

--- src/View/subView/ISubscriber.ts
+++ src/View/subView/ISubscriber.ts
@@ -2,7 +2,6 @@ interface ISubscriber{
     mouseMoveButton(): void;
     mouseUp(): void;
     mouseMoveButton_2(): void;
-    mouseUp_2(): void;
-      
+    mouseUp_2(): void;      
 }
 export default ISubscriber
