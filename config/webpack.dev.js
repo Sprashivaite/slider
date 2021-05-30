228008f5724e98ab0d2bb43e90352243d81a1443
Sprashivaite <sanitar.kem@mail.ru>
2021-05-30 18:14:18 +0700

    changed devtool

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -6,7 +6,7 @@ const webpack = require('webpack')
 
 module.exports = merge(common, {
   mode: 'development',
-  // devtool: 'inline-source-map',
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, '../dist'),
   },
