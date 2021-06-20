e175311d76b403bab0715869152410a598e99eae
Sprashivaite <sanitar.kem@mail.ru>
2021-06-21 03:25:08 +0700

    refactoring

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -3,14 +3,15 @@ import { DEFAULT_MODEL_CONFIG } from '../defaults';
 import { ViewHandleData, elementsSize, ModelConfig } from '../types';
 
 class Model extends Observer {
-  config!: ModelConfig;
+  private config!: ModelConfig;
 
-  shift!: number;
+  private shift: number;
 
-  elementsSize!: { fieldSize: number; buttonSize: number };
+  private elementsSize!: { fieldSize: number; buttonSize: number };
 
   constructor(config = DEFAULT_MODEL_CONFIG as ModelConfig) {
-    super();
+    super();    
+    this.shift = 0;
     this.init(config);
   }
 
@@ -39,10 +40,13 @@ class Model extends Observer {
 
     const shiftLeft: number = mouseCoords - this.shift;
 
-    if (shiftLeft >= fieldSize)
+    if (shiftLeft >= fieldSize) {
       this.updateButton({ ...data, value: fieldSize });
-    else if (shiftLeft <= 0) this.updateButton({ ...data, value: 0 });
-    else this.updateButton({ ...data, value: shiftLeft });
+    } else if (shiftLeft <= 0) {
+      this.updateButton({ ...data, value: 0 });
+    } else {
+      this.updateButton({ ...data, value: shiftLeft });
+    }
   }
 
   calcStopPointPX(data: ViewHandleData): void {
@@ -51,34 +55,28 @@ class Model extends Observer {
     const { fieldSize } = this.elementsSize;
 
     const stepPX: number = (fieldSize * step) / (max - min);
+    let stopPoint = fieldSize
 
-    const arrStopPoints: number[] = [];
     for (let i = 0; i <= fieldSize; i += stepPX) {
-      i = Number(i.toFixed(2));
-      arrStopPoints.push(i);
+      const isLessThenStep = buttonOffset <= i + stepPX / 2      
+      if (isLessThenStep) {
+        stopPoint = i
+        break;
+      }
     }
-
-    let stopPoint: number | undefined = arrStopPoints.find(
-      (item) => buttonOffset <= item + stepPX / 2
-    );
-    if (stopPoint === undefined) stopPoint = fieldSize;
-
     this.updateButton({ ...data, value: stopPoint });
   }
 
-  calcFlagValue(data: ViewHandleData): number {
+  calcFlagValue(buttonOffset: number): number {
     const { max, min } = this.config;
-    const { buttonOffset } = data;
+    const offset = buttonOffset;
     const { fieldSize } = this.elementsSize;
 
-    const part: number = min + (buttonOffset * (max - min)) / fieldSize;
+    const value: number = min + (offset * (max - min)) / fieldSize;
+    const roundedValue: number = this.roundByStep(value)
 
-    const digitsAfterDot: number = this.calcDigitsAfterDot();
-    const fixedPart = Number(part.toFixed(digitsAfterDot));
-
-    const result: number = this.findNearestValue(fixedPart);
-    const roundedResult = Number(result.toFixed(this.calcDigitsAfterDot()));
-    return roundedResult;
+    if (roundedValue === max) return roundedValue
+    return this.findNearestValue(roundedValue);
   }
 
   moveToValue(data: ViewHandleData): void {
@@ -94,18 +92,17 @@ class Model extends Observer {
   calcScaleValues(): void {
     const { max, min, scaleQuantity } = this.config;
 
-    let stepValue = min;
-    const stepValues = (max - min) / (scaleQuantity - 1);
-
-    const digitsAfterDot: number = this.calcDigitsAfterDot();
-    const stepValuesFixed = Number(stepValues.toFixed(digitsAfterDot));
+    const scaleStep = (max - min) / (scaleQuantity - 1);
 
     const scaleValues: Array<number> = [];
-    for (let i = 0; i < scaleQuantity - 1; i += 1) {
-      const result = this.findNearestValue(stepValue);
-      scaleValues.push(Number(result.toFixed(digitsAfterDot)));
-      stepValue += stepValuesFixed;
+    let stepValue: number = min;
+    let result: number;
+
+    for (stepValue; stepValue < max; stepValue += scaleStep) {
+      result = this.findNearestValue(stepValue);
+      scaleValues.push(result);
     }
+    if (scaleValues.length >= scaleQuantity) scaleValues.length = scaleQuantity - 1
     scaleValues.push(max);
 
     this.emit('scaleUpdate', { scaleValues, quantity: scaleQuantity });
@@ -114,38 +111,41 @@ class Model extends Observer {
   private init(config: ModelConfig): void {
     this.config = { ...DEFAULT_MODEL_CONFIG, ...config };
     this.validate();
-    this.shift = 0;
   }
 
   private validate(): void {
-    let { max, min, step, scaleQuantity } = this.config;
+    this.validateMinMax();
+    this.validateStep();
+    this.validateScaleQuantity();
+  }
 
+  private validateMinMax(): void {
+    let { max, min } = this.config;
     if (typeof min !== 'number') min = 0;
     if (min >= max) min = max - 1;
     if (typeof max !== 'number') max = 100;
     if (max <= min) max = min + 1;
-    step = this.validateStep();
-    scaleQuantity = this.validateScaleQuantity();
-    this.config = { max, min, step, scaleQuantity };
+    this.config = {...this.config, max, min };
   }
 
-  private validateStep(): number {
-    const { max, min, step } = this.config;
-    let invalidStep = step;
-    if (typeof invalidStep !== 'number') invalidStep = 1;
-    if (invalidStep >= max - min) invalidStep = max - min;
-    if (invalidStep <= 0) invalidStep = 1;
+  private validateStep(): void {
+    const { max, min } = this.config;
+    let { step } = this.config;
+    
+    if (typeof step !== 'number') step = 1;
+    if (step >= max - min) step = max - min;
+    if (step <= 0) step = 1;
 
     const validateLargeNumbers = (value: number): number => {
       if (value * 1000 >= max - min) return value;
       return validateLargeNumbers(value * 10);
     };
 
-    const validStep = validateLargeNumbers(invalidStep);
-    return validStep;
+    step = validateLargeNumbers(step);
+    this.config = {...this.config, step };
   }
 
-  private validateScaleQuantity(): number {
+  private validateScaleQuantity(): void {
     const { max, min, step } = this.config;
     let { scaleQuantity } = this.config;
 
@@ -156,41 +156,42 @@ class Model extends Observer {
 
     if (quantitySteps < scaleQuantity) scaleQuantity = quantitySteps;
     scaleQuantity = Number(scaleQuantity.toFixed(0));
-    return scaleQuantity;
+
+    this.config = {...this.config, scaleQuantity };
   }
 
   private updateButton(data: ViewHandleData): void {
     const { button, value } = data;
-    const flagValue = this.calcFlagValue(data);
+    const flagValue = this.calcFlagValue(value!);
     if (!button.previousElementSibling) {
-      this.emit('updateFirstButtonValue', flagValue);
       this.emit('updateFirstButtonPX', value);
+      this.emit('updateFirstButtonValue', flagValue);
     } else {
-      this.emit('updateSecondButtonValue', flagValue);
       this.emit('updateSecondButtonPX', value);
+      this.emit('updateSecondButtonValue', flagValue);
     }
   }
 
-  private calcDigitsAfterDot(): number {
+  private roundByStep(value: number): number {
     const { step } = this.config;
     const isDot = step.toString().includes('.');
-    const digitsAfterDot = step.toString().split('.').pop()!.length;
-    return isDot ? digitsAfterDot : 0;
+    const digitsAfterDot = String(step).split('.').pop()!.length;
+
+    return isDot ? Number(value.toFixed(digitsAfterDot)) : Number(value.toFixed(0));
   }
 
   private findNearestValue(value: number): number {
     const { max, min, step } = this.config;
 
-    const steps: number[] = [];
-    for (let i = min; i <= max; i += step) steps.push(i);
+    const roundedValue = this.roundByStep(value)
+    let result = max;
 
-    let nearestValue: number | undefined = steps.find((item, index, array) => {
-      const halfStep = value + step / 2;
+    for (let i = min; i <= max; i += step) {
+      i = this.roundByStep(i)
+      if (roundedValue >= i) result = i
+    }
 
-      return halfStep >= item && halfStep < array[index + 1];
-    });
-    if (nearestValue === undefined) nearestValue = steps.pop()!;
-    return nearestValue;
+    return this.roundByStep(result);
   }
 }
 export default Model;
